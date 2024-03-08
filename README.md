815ffb3a3c2eb99ad8dafaeb603142da5b1cc496
nowfeel <nowfeel@bliink.io>
2024-03-08 17:42:48 +0100

    feat: instanciate connection to sqlalchemy with mysql database using driver doc: update readme

--- README.md
+++ README.md
@@ -8,3 +8,20 @@
 ```shell
 source env/bin/activate
 ``` 
+
+## Set up the database
+
+WARN : We use MySQL for this project
+We work in local, so please before setting up the database for this project, be sure to have correctly installed MySQL
+on your computer or using Docker.
+
+An env file is necessary to connect to the database so let's create this env file at the root of the project:
+```shell
+touch .env
+```
+
+then you will need to set few variables:
+- "DB_PORT" 
+- "DB_USER"
+- "DB_PASSWORD"
+- "DB_NAME"
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("main.py")
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+db_url = f"mysql+mysqlconnector://{db_user}:{db_password}@localhost:{db_port}/{db_name}"
+
+
+def connect_to_database(url: str):
+    engine = create_engine(url)
+    try:
+        connection = engine.connect()
+        print(f'connected: {connection}')
+    except Exception as e:
+        return e
+
+
+db_connection = connect_to_database(db_url)
