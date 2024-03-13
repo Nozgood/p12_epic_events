b80daf524e63459c7e8851bf948adfec3dd93001
nowfeel <nowfeel@bliink.io>
2024-03-13 07:28:11 +0100

    feat: add relationship on models

--- models/models.py
+++ models/models.py
@@ -14,7 +14,9 @@ class Collaborator(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String, unique=True, index=True)  # => Hash the password (lib / plugin?)
 
-    clients = relationship("Client", back_populates="contact")
+    clients = relationship("Client", back_populates="contact")  # back_populates = relation inverse
+    deals = relationship("Deal", back_populates="contact")
+    events = relationship("Event", back_populates="contact")
 
 
 class Client(Base):
@@ -27,19 +29,27 @@ class Client(Base):
     corporation = Column(String, unique=False, index=True)
     created_at = Column(DateTime, unique=False, index=True)
     updated_at = Column(DateTime, unique=False, index=True)
-    contact = Column(Integer, ForeignKey("collaborators.id"))  # it must be a commercial role
+    contact_id = Column(Integer, ForeignKey("collaborators.id"))  # it must be a commercial role
+
+    contact = relationship("Collaborator", back_populates="clients")
+    deals = relationship("Deal", back_populates="client")
+    events = relationship("Event", back_populates="client")
 
 
 class Deal(Base):
     __tablename__ = 'deals'
 
     id = Column(UUID, primary_key=True, index=True)
-    client = Column(Integer, ForeignKey("clients.id"))
-    contact = Column(Integer, ForeignKey("collaborators.id"))
+    client_id = Column(Integer, ForeignKey("clients.id"))
+    contact_id = Column(Integer, ForeignKey("collaborators.id"))
     bill = Column(Float, unique=False)
     remaining_on_bill = Column(Float, unique=False)
     created_at = Column(DateTime, unique=False, index=True)
     has_been_signed = Column(Boolean, unique=False, index=True)
+
+    contact = relationship("Collaborator", back_populates="deals")
+    client = relationship("Client", back_populates="deals")
+    events = relationship("Event", back_populates="deal")
 
 
 class Event(Base):
@@ -47,11 +57,16 @@ class Event(Base):
 
     id = Column(UUID, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
-    deal = Column(Integer, ForeignKey("contacts.id"))
-    client = Column(Integer, ForeignKey("clients.id"))
+    deal_id = Column(Integer, ForeignKey("contacts.id"))
+    client_id = Column(Integer, ForeignKey("clients.id"))
+    contact_id = Column(Integer, ForeignKey("collaborators.id"))  # => it must be a support role
     start_date = Column(DateTime, unique=False, index=True)
     end_date = Column(DateTime, unique=False, index=True)
-    contact = Column(Integer, ForeignKey("collaborators.id"))  # => it must be a support role
     location = Column(String, unique=False, index=True)
     attendees = Column(Integer, unique=False, index=False)
     notes = Column(String, unique=False, index=False)
+
+    contact = relationship("Collaborator", back_populates="events")
+    client = relationship("Client", back_populates="events")
+    deal = relationship("Deal", back_populates="events")
+
